9096b32ec69e94ac74ef063ce7f265096bb76247
Martin-LeeHsinHsueh <69800820+Martin-LeeHsinHsueh@users.noreply.github.com>
2020-08-17 21:02:53 +0800

    initialization

---
+++ script.js
@@ -0,0 +1,32 @@
+var button = document.getElementById("create");
+var input = document.getElementById("input");
+var list = document.getElementById("list");
+var todolist = JSON.parse(localStorage.getItem("listItems")) || [];
+render();
+
+function addData(){
+  if(input.value!=""){
+    todolist.push(input.value);
+    input.value="";
+  
+    render()
+  }
+}
+
+function delData(i){
+  //刪除資料
+  todolist.splice(i,1);
+  
+  
+  //更新顯示畫面
+  render();
+}
+
+function render(){
+  localStorage.setItem("listItems",JSON.stringify(todolist))
+  var content = "";
+  for(var i=0; i<todolist.length; i++){
+    content = content + "<div>" + "<button class='delete' onclick='delData("+i+")'>刪除</button>" + todolist[i] + "</div>";
+  }
+  list.innerHTML=content;  
+}
